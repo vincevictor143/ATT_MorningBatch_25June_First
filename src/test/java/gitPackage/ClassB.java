fbf9fed1234f7a812e2a96068a4db4c4dde2bd46
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-08-31 07:54:01 +0530

    Vince:Class B has been added

---
+++ src/test/java/gitPackage/ClassB.java
@@ -0,0 +1,10 @@
+package gitPackage;
+
+public class ClassB 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is the class B for understanding the concept of pull code");
+	}
+
+}
